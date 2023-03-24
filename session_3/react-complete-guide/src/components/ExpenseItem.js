d9a04ca2538c99430696d158793a4753143ef91f
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-03-24 13:47:37 +0100

    Add jsx

--- session_3/react-complete-guide/src/components/ExpenseItem.js
+++ session_3/react-complete-guide/src/components/ExpenseItem.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import "./ExpenseItem.css"
 
 function ExpenseItem() {
-  return (
-    <div>ExpenseItem</div>
+  return(
+  <>
+    <div>March 28th 2021</div>
+    <div>
+      <h2>Car Insurance</h2>
+      <div>$294.67</div>
+    </div>
+  </>
   )
 }
 
-export default ExpenseItem
+export default ExpenseItem;
