af015d103a6d16a3dd3af081c7cf5e03fd4d36fa
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-03-24 16:33:48 +0100

    Create Expenses comp

---
+++ session_3/react-complete-guide/src/components/Expenses.js
@@ -0,0 +1,31 @@
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+
+const Expenses = ({expenses}) => {
+  return (
+    <div className="expenses">
+         <ExpenseItem
+        date={expenses[0].date}
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+      />
+      <ExpenseItem
+        date={expenses[1].date}
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+      />
+      <ExpenseItem
+        date={expenses[2].date}
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+      />
+      <ExpenseItem
+        date={expenses[3].date}
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+      />
+    </div>
+  )
+}
+
+export default Expenses
