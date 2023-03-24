f6632327aad742107ee275d2d7fccb45ca0ddec8
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-03-24 15:41:26 +0100

    Pass props

--- session_3/react-complete-guide/src/App.js
+++ session_3/react-complete-guide/src/App.js
@@ -25,10 +25,26 @@ function App() {
 
   return (
     <div>
-      <ExpenseItem />
-      <ExpenseItem />
-      <ExpenseItem />
-      <ExpenseItem />
+      <ExpenseItem
+        date={expenses[0].date}
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+      />
+      <ExpenseItem
+        date={expenses[1].date}
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+      />
+      <ExpenseItem
+        date={expenses[2].date}
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+      />
+      <ExpenseItem
+        date={expenses[3].date}
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+      />
     </div>
   );
 }
