dbcd5a6f8c32c3229fb83f841920feeecd30b5df
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-03-24 13:51:17 +0100

    Add classes

--- session_3/react-complete-guide/src/components/ExpenseItem.js
+++ session_3/react-complete-guide/src/components/ExpenseItem.js
@@ -2,13 +2,13 @@ import "./ExpenseItem.css"
 
 function ExpenseItem() {
   return(
-  <>
+  <div className="expense-item">
     <div>March 28th 2021</div>
-    <div>
+    <div className="expense-item__description">
       <h2>Car Insurance</h2>
-      <div>$294.67</div>
+      <div className="expense-item__price">$294.67</div>
     </div>
-  </>
+  </div>
   )
 }
 
