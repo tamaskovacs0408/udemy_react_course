5a66326a296f064b9723a53441bf17a692640945
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-03-24 16:01:16 +0100

    Rearrange date

--- session_3/react-complete-guide/src/components/ExpenseItem.js
+++ session_3/react-complete-guide/src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem({ date, title, amount }) {
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const year = date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{date.toISOString().slice(0, 10)}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
