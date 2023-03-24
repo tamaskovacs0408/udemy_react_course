a80918fa6152549a49cb1a89ffdaaac9b1352b35
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-03-24 16:23:59 +0100

    Add classes

--- session_3/react-complete-guide/src/components/ExpenseDate.js
+++ session_3/react-complete-guide/src/components/ExpenseDate.js
@@ -1,13 +1,15 @@
+import "./ExpenseDate.css"
+
 const ExpenseDate = ({ date }) => {
   const month = date.toLocaleString("en-US", { month: "long" });
   const day = date.toLocaleString("en-US", { day: "2-digit" });
   const year = date.getFullYear();
 
   return (
-    <div>
-      <div>{month}</div>
-      <div>{day}</div>
-      <div>{year}</div>
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{day}</div>
+      <div className="expense-date__day">{year}</div>
     </div>
   );
 };
