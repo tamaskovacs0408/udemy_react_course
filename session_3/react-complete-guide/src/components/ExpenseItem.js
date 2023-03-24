a756f2e729f058f995ef8d2be09cbf489b11e465
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-03-24 14:03:20 +0100

    Update component with "dynamic" data

--- session_3/react-complete-guide/src/components/ExpenseItem.js
+++ session_3/react-complete-guide/src/components/ExpenseItem.js
@@ -1,15 +1,19 @@
-import "./ExpenseItem.css"
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
-  return(
-  <div className="expense-item">
-    <div>March 28th 2021</div>
-    <div className="expense-item__description">
-      <h2>Car Insurance</h2>
-      <div className="expense-item__price">$294.67</div>
+  const expenseDate = new Date(2023, 3, 24);
+  const expenseTitle = "Car Insurance";
+  const expensePrice = 294.67;
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString().slice(0, 10)}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expensePrice}</div>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
 export default ExpenseItem;
