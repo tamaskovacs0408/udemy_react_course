6dfd42ed7c36074a4c97e892913b7d2d166f130f
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-03-26 16:46:13 +0200

    Import useState hook

--- session_3/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ session_3/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,19 @@
+import { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem({ date, title, amount }) {
+  const [newTitle, setNewTitle] = useState(title);
 
   return (
-      <Card className="expense-item">
-        <ExpenseDate date={date} />
-        <div className="expense-item__description">
-          <h2>{title}</h2>
-          <div className="expense-item__price">${amount}</div>
-        </div>
-      </Card>
+    <Card className="expense-item">
+      <ExpenseDate date={date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+    </Card>
   );
 }
 export default ExpenseItem;
