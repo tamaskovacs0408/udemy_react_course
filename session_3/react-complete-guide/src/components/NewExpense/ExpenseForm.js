78123671b11a41f7700a7599e2082b94e5a72a5a
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-03-28 10:28:16 +0200

    Add two way binding

--- session_3/react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ session_3/react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -40,8 +40,9 @@ const ExpenseForm = () => {
       amount: enteredAmount,
       date: new Date(enteredDate)
     }
-
-    console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   }
 
   return (
@@ -49,7 +50,7 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={handleTitleInput} />
+          <input type="text" value={enteredTitle} onChange={handleTitleInput} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -57,6 +58,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -66,6 +68,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
