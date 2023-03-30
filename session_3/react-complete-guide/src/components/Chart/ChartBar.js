6177be3cc96b0a84572576239e1543e25ec7c19f
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-03-30 15:42:18 +0200

    Set up chart bar

---
+++ session_3/react-complete-guide/src/components/Chart/ChartBar.js
@@ -0,0 +1,18 @@
+import "./ChartBar.css";
+
+const ChartBar = ({value, maxValue, label}) => {
+    let barFillHeight = "0%";
+
+    if (maxValue > 0) {
+        barFillHeight = Math.round((value / maxValue) * 100)
+    }
+
+  return <div className="chart-bar">
+    <div className="chart-bar__inner">
+        <div className="chart-bar__fill" style={{height: barFillHeight}}></div>
+        <div className="chart-bar__label">{label}</div>
+    </div>
+  </div>;
+};
+
+export default ChartBar;
