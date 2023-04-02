b85c16b6bd511b856a89ff1623e5f98dccfc4a6a
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-04-02 11:37:41 +0200

    Update component

--- section_8/src/Components/Users/AddUser.js
+++ section_8/src/Components/Users/AddUser.js
@@ -1,16 +1,22 @@
+import Card from "../UI/Card";
+
+import style from "./AddUser.module.css";
+
 const AddUser = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card outerClass={style.input}>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
