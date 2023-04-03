fb606d43c65960354449f539654d68cdfcb5f260
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-04-03 10:35:00 +0200

    Import component

--- section_8/src/App.js
+++ section_8/src/App.js
@@ -1,8 +1,10 @@
 import AddUser from "./Components/Users/AddUser";
+import UsersList from "./Components/Users/UsersList";
 function App() {
   return (
     <div>
       <AddUser />
+      <UsersList users={[]}/>
     </div>
   );
 }
