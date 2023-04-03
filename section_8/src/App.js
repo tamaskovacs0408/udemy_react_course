89a45363d7e1879fd58cbb142cd432d8db7412ac
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-04-03 10:49:40 +0200

    Add function to lift up states

--- section_8/src/App.js
+++ section_8/src/App.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
+
 import AddUser from "./Components/Users/AddUser";
 import UsersList from "./Components/Users/UsersList";
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const handleAddUser = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, {name: uName, age: uAge, id: Math.random().toString()}];
+    })
+  }
+
   return (
     <div>
-      <AddUser />
-      <UsersList users={[]}/>
+      <AddUser onAddUser={handleAddUser}/>
+      <UsersList users={usersList}/>
     </div>
   );
 }
