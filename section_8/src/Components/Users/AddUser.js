ee87bc106c1b94cc73fc5dde8aca31d5a9de3e47
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-04-04 16:48:35 +0200

    Change states to refs

--- section_8/src/Components/Users/AddUser.js
+++ section_8/src/Components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
@@ -7,17 +7,16 @@ import ErrorModal from "../UI/ErrorModal";
 import style from "./AddUser.module.css";
 
 const AddUser = ({ onAddUser }) => {
-  const [enterUsername, setEnterUsername] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
-  const [error, setError] = useState();
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
 
-  const handleUsernameChange = (e) => {
-    setEnterUsername(e.target.value);
-  };
+  const [error, setError] = useState();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (enterUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    const enteredName = nameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
+    if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
         title: "Invalid input",
         message: "Please enter a valid name.",
@@ -29,9 +28,7 @@ const AddUser = ({ onAddUser }) => {
         message: "Please enter a valid age.",
       });
     }
-    onAddUser(enterUsername, enteredAge);
-    setEnterUsername("");
-    setEnteredAge("");
+    onAddUser(enteredName, enteredAge);
   };
 
   const handleError = () => {
@@ -50,19 +47,9 @@ const AddUser = ({ onAddUser }) => {
       <Card outerClass={style.input}>
         <form onSubmit={handleSubmit}>
           <label htmlFor="username">Username</label>
-          <input
-            id="username"
-            type="text"
-            onChange={handleUsernameChange}
-            value={enterUsername}
-          />
+          <input id="username" type="text" ref={nameInputRef} />
           <label htmlFor="age">Age (Years)</label>
-          <input
-            id="age"
-            type="number"
-            onChange={(e) => setEnteredAge(e.target.value)}
-            value={enteredAge}
-          />
+          <input id="age" type="number" ref={ageInputRef}/>
           <Button type="submit">Add User</Button>
         </form>
       </Card>
