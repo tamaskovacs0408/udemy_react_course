e021c6ca05826896888e637da517eeff8ea56cba
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-04-04 16:21:13 +0200

    Set up portals

--- section_8/src/Components/UI/ErrorModal.js
+++ section_8/src/Components/UI/ErrorModal.js
@@ -1,23 +1,43 @@
+import ReactDOM from "react-dom";
 import Card from "./Card";
 import Button from "./Button";
 import style from "./ErrorModal.module.css";
 
+const Backdrop = ({ onCloseModal }) => {
+  return <div className={style.backdrop} onClick={onCloseModal} />;
+};
+
+const ModalOverlay = ({ title, message, onCloseModal }) => {
+  return (
+    <Card outerClass={style.modal}>
+      <header className={style.header}>
+        <h2>{title}</h2>
+      </header>
+      <div className={style.content}>
+        <p>{message}</p>
+      </div>
+      <footer className={style.actions}>
+        <Button onClick={onCloseModal}>Close</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = ({ title, message, onCloseModal }) => {
   return (
     <>
-      <div className={style.backdrop} onClick={onCloseModal}>
-        <Card outerClass={style.modal}>
-          <header className={style.header}>
-            <h2>{title}</h2>
-          </header>
-          <div className={style.content}>
-            <p>{message}</p>
-          </div>
-          <footer className={style.actions}>
-            <Button onClick={onCloseModal}>Close</Button>
-          </footer>
-        </Card>
-      </div>
+      {ReactDOM.createPortal(
+        <Backdrop onCloseModal={onCloseModal} />,
+        document.querySelector("#backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={title}
+          message={message}
+          onCloseModal={onCloseModal}
+        />,
+        document.querySelector("#modal-root")
+      )}
     </>
   );
 };
