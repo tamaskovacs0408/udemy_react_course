06d4c66a080de16d2a581965a5a5955f4962e753
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-04-06 14:45:40 +0200

    Set up useContext

--- section_10/src/components/MainHeader/Navigation.js
+++ section_10/src/components/MainHeader/Navigation.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { useContext } from "react";
 
-import classes from './Navigation.module.css';
+import AuthContext from "../../store/auth-context";
+import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
+  const authCtx = useContext(AuthContext);
+
   return (
     <nav className={classes.nav}>
       <ul>
-        {props.isLoggedIn && (
+        {authCtx.isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {authCtx.isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {authCtx.isLoggedIn && (
           <li>
             <button onClick={props.onLogout}>Logout</button>
           </li>
