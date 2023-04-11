0f188447968e2e6ad1a4cbd7157cc33e3c8489f2
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-04-11 11:17:57 +0200

    Manage contexts

--- section_10/src/store/auth-context.js
+++ section_10/src/store/auth-context.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
-
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {}
 });
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "true");
+    setIsLoggedIn(true);
+  };
+
+  useEffect(() => {
+    const storedUserLogin = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLogin === "true") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const logoutHandler = () => {
+    // localStorage.setItem("isLoggedIn", "false");
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  return <AuthContext.Provider value={{
+    isLoggedIn: isLoggedIn,
+    onLogin: loginHandler,
+    onLogout: logoutHandler
+  }}>
+    {props.children}
+    </AuthContext.Provider>;
+};
 
 export default AuthContext;
