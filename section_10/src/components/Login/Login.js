021498f7357704216342a390baaddc67a15d57f1
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-04-11 13:47:09 +0200

    update

--- section_10/src/components/Login/Login.js
+++ section_10/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer, useContext } from "react";
+import React, { useState, useRef, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
@@ -27,6 +27,9 @@ const Login = () => {
   });
 
   const authCtx = useContext(AuthContext);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   // useEffect(() => {
   //   const identifier = setTimeout(() => {
@@ -62,13 +65,20 @@ const Login = () => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+    if (formIsValid) {
+      authCtx.onLogin(emailState.value, passwordState.value);
+    } else if (!emailState.isValid) {
+        emailInputRef.current.focus();
+    } else {
+        passwordInputRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailInputRef}
           id="email"
           label="E-mail"
           type="email"
@@ -78,6 +88,7 @@ const Login = () => {
           onBlur={validateEmailHandler}
         />
         <Input
+          ref={passwordInputRef}
           type="password"
           id="password"
           value={passwordState.value}
@@ -86,7 +97,7 @@ const Login = () => {
           onBlur={validatePasswordHandler}
         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
--- section_10/src/components/UI/Input/Input.js
+++ section_10/src/components/UI/Input/Input.js
@@ -1,22 +1,36 @@
+import { useRef, forwardRef ,useImperativeHandle } from "react";
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const focus = () => {
+    inputRef.current.focus();
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+        focus: focus
+    }
+  })
+
   return (
     <div
-    className={`${classes.control} ${
-      props.isValid === false ? classes.invalid : ""
-    }`}
-  >
-    <label htmlFor={props.id}>{props.label}</label>
-    <input
-      type={props.type}
-      id={props.id}
-      value={props.value}
-      onChange={props.onChange}
-      onBlur={props.onBlur}
-    />
-  </div>
-  )
-}
+      className={`${classes.control} ${
+        props.isValid === false ? classes.invalid : ""
+      }`}
+    >
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        ref={inputRef}
+        type={props.type}
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+    </div>
+  );
+});
 
-export default Input
+export default Input;
