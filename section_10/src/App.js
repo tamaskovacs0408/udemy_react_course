cc1977e9f6d31316b6fe62dab2e43d19b9140902
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-04-11 11:17:05 +0200

    Add contexts

--- section_10/src/App.js
+++ section_10/src/App.js
@@ -1,46 +1,21 @@
-import { useState, useEffect } from "react";
+import { useContext } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
-
 import AuthContext from "./store/auth-context";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem("isLoggedIn", "true");
-    setIsLoggedIn(true);
-  };
-
-  useEffect(() => {
-    const storedUserLogin = localStorage.getItem("isLoggedIn");
-
-    if (storedUserLogin === "true") {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const logoutHandler = () => {
-    // localStorage.setItem("isLoggedIn", "false");
-    localStorage.removeItem("isLoggedIn");
-    setIsLoggedIn(false);
-  };
+  const authCtx = useContext(AuthContext)
 
   return (
-      <AuthContext.Provider value={{
-        isLoggedIn: isLoggedIn,
-        onLogout: logoutHandler
-    }}>
-        <MainHeader />
-        <main>
-          {!isLoggedIn && <Login onLogin={loginHandler} />}
-          {isLoggedIn && <Home onLogout={logoutHandler} />}
-        </main>
-      </AuthContext.Provider>
+    <>
+      <MainHeader />
+      <main>
+        {!authCtx.isLoggedIn && <Login />}
+        {authCtx.isLoggedIn && <Home />}
+      </main>
+    </>
   );
 }
 
