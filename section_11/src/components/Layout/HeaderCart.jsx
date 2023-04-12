1765c516bb09f7760520a066513aa4e9b16a74a4
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-04-12 09:56:28 +0200

    Add button animation logic

--- section_11/src/components/Layout/HeaderCart.jsx
+++ section_11/src/components/Layout/HeaderCart.jsx
@@ -1,26 +1,44 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCart.module.css";
 
-const HeaderCart = ({showCart}) => {
+const HeaderCart = ({ showCart }) => {
+  const [btnIsHighLighted, setBtnIsHighLighted] = useState(false);
   const cartCtx = useContext(CartContext);
+
+  const { items } = cartCtx;
 
   const numOfCartItems = cartCtx.items.reduce((currNum, item) => {
     return currNum + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighLighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHighLighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighLighted(false);
+    }, 300);
+    
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={showCart}>
+    <button className={btnClasses} onClick={showCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
-      <span>
-        Your cart
-      </span>
-      <span className={classes.badge}>
-        {numOfCartItems}
-      </span>
+      <span>Your cart</span>
+      <span className={classes.badge}>{numOfCartItems}</span>
     </button>
   );
 };
