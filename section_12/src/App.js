ab81b0780ee8fc1458f649673c1c0401228ad3db
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-04-13 15:24:15 +0200

    Add useCallback

--- section_12/src/App.js
+++ section_12/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import Button from "./components/UI/Button/Button";
 import DemoOutput from "./Demo/DemoOutput";
 
@@ -7,14 +7,16 @@ import "./App.css";
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
 
-  const handleToggleParagraph = () => {
+  const handleToggleParagraph = useCallback(() => {
     setShowParagraph((prevParagraph) => !prevParagraph);
-  };
+  }, []);
+
+  console.log("App Running");
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph}/>
+      <DemoOutput show={false}/>
       <Button onClick={handleToggleParagraph}>Toggle paragraph</Button>
     </div>
   );
