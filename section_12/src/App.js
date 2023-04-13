185d5dafa17470bd679bc999cd0a5fa5306c34c6
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-04-13 13:52:26 +0200

    Update

--- section_12/src/App.js
+++ section_12/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import { useState } from "react";
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./Demo/DemoOutput";
 
-import './App.css';
+import "./App.css";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const handleToggleParagraph = () => {
+    setShowParagraph((prevParagraph) => !prevParagraph);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph}/>
+      <Button onClick={handleToggleParagraph}>Toggle paragraph</Button>
     </div>
   );
 }
