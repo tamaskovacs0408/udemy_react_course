7a0f5aa6a183ed8a90b747edd90849e9d664de55
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-04-23 13:58:24 +0200

    Update date

--- section_14/src/components/Movie.js
+++ section_14/src/components/Movie.js
@@ -3,10 +3,12 @@ import React from 'react';
 import classes from './Movie.module.css';
 
 const Movie = (props) => {
+  let releaseDate = props.releaseDate.replace(/-/g, ".");
+
   return (
     <li className={classes.movie}>
       <h2>{props.title}</h2>
-      <h3>{props.releaseDate}</h3>
+      <h3>{releaseDate}</h3>
       <p>{props.openingText}</p>
     </li>
   );
