82bbc8ebcf0569b88b1bcfdc20542d66a06a0133
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-04-24 21:11:46 +0200

    Update app with basic error handling

--- section_14/src/App.js
+++ section_14/src/App.js
@@ -6,12 +6,22 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleFetch = async () => {
-    setIsLoading(prevState => !prevState);
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
-    setMovies(data.results);
+    setIsLoading((prevState) => !prevState);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films");
+      if (!response.ok) {
+        throw new Error("Something went wrong.");
+      }
+      const data = await response.json();
+      setMovies(data.results);
+      
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading((prevState) => !prevState);
   };
 
@@ -31,6 +41,7 @@ function App() {
         ) : (
           <MoviesList movies={movies} />
         )}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </>
   );
