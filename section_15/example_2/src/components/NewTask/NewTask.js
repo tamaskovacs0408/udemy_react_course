7a4742bb26b9aef4529ac60eda7202dc86893f5f
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-05-04 20:34:05 +0200

    Add new backend links

--- section_15/example_2/src/components/NewTask/NewTask.js
+++ section_15/example_2/src/components/NewTask/NewTask.js
@@ -12,12 +12,12 @@ const NewTask = (props) => {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json',
+        "https://react-http-f2e3a-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
         {
-          method: 'POST',
+          method: "POST",
           body: JSON.stringify({ text: taskText }),
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
         }
       );
