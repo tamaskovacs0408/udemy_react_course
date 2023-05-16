0c3e85e2ce661e93a09d82f020fa48533ea61c5d
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-05-16 15:50:36 +0200

    Handling name input and form submit

--- section_16/src/components/SimpleInput.js
+++ section_16/src/components/SimpleInput.js
@@ -1,9 +1,32 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputRef = useRef();
+
+  const handleEnteredName = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(nameInputRef.current.value);
+    setEnteredName("");
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={handleFormSubmit}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={handleEnteredName}
+          ref={nameInputRef}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
