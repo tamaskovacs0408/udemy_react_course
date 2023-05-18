73c74143b1946e9864a9718a26e57ee82671ba18
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-05-18 15:59:45 +0200

    Create custom hook for handling inputs

---
+++ section_16/src/hooks/UseInput.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const UseInput = (validateValue) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+
+  const valueIsValid = validateValue(enteredValue);
+  const hasError = !valueIsValid && isTouched;
+
+  const valueChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const inputBlurHandler = () => {
+    isTouched(true);
+  };
+
+  return {
+    value: enteredValue,
+    hasError,
+    valueChangeHandler,
+    inputBlurHandler
+  };
+};
+
+export default UseInput;
