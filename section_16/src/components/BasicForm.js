631005620348a2e099e889d7a1739054368d48ed
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-05-20 17:29:45 +0200

    Add customhooks form handle inputs

--- section_16/src/components/BasicForm.js
+++ section_16/src/components/BasicForm.js
@@ -1,21 +1,50 @@
+import useInput from "../hooks/use-input";
+
 const BasicForm = (props) => {
+  const {
+    value: enteredFirstName,
+    isValid: enteredFirstNameIsValid,
+    hasError: firstNameInputHasError,
+    valueChangeHandler: firstNameChangeHandler,
+    inputBlurHandler: firstNameBlurHandler,
+    reset: resetFirstNameInput,
+  } = useInput((value) => value.trim() !== 0);
+
+  const {
+    value: enteredLastName,
+    isValid: enteredLastNameIsValid,
+    hasError: lastNameInputHasError,
+    valueChangeHandler: lastNameChangeHandler,
+    inputBlurHandler: lastNameBlurHandler,
+    reset: resetLastNameInput,
+  } = useInput((value) => value.trim() !== 0);
+
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailInputHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmailInput,
+  } = useInput((value) => value.includes("@"));
+
   return (
     <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+      <div className="control-group">
+        <div className="form-control">
+          <label htmlFor="name">First Name</label>
+          <input type="text" id="name" />
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className="form-control">
+          <label htmlFor="name">Last Name</label>
+          <input type="text" id="name" />
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">E-Mail Address</label>
+        <input type="text" id="name" />
       </div>
-      <div className='form-actions'>
+      <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
