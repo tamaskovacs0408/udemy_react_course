d4967e08fb73cf14d4c02e57f0151b5d10de95a4
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-05-22 16:08:28 +0200

    Fetching burgers from firebase

--- section_11/src/components/Meals/AvailableMeals.jsx
+++ section_11/src/components/Meals/AvailableMeals.jsx
@@ -1,14 +1,40 @@
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
-import { DUMMY_MEALS } from "../../data/dumBurgers";
 import classes from "./AvailableMeals.module.css";
 
 function AvailableMeals() {
+  const [burgers, setBurgers] = useState([]);
+
+  useEffect(() => {
+    const fetchBurgers = async () => {
+      const response = await fetch("https://burgersdata-default-rtdb.europe-west1.firebasedatabase.app/burgers.json");
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push(
+          {
+            id: key,
+            name: responseData[key].name,
+            image: responseData[key].image,
+            description: responseData[key].description,
+            price: responseData[key].price
+          }
+        )
+      }
+      setBurgers(loadedMeals);
+    }
+
+    fetchBurgers();
+  }, [])
   return (
     <section className={classes.meals}>
       <Card>
         <ul>
-          {DUMMY_MEALS.map((burger) => {
+          
+          {burgers.map((burger) => {
             return (
               <MealItem key={burger.id}
                 id={burger.id}
