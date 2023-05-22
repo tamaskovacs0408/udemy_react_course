504e0da85bb9aa6ea84515f5e6940fd7f3951c26
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-05-22 16:19:16 +0200

    Add loading state

--- section_11/src/components/Meals/AvailableMeals.jsx
+++ section_11/src/components/Meals/AvailableMeals.jsx
@@ -5,38 +5,49 @@ import classes from "./AvailableMeals.module.css";
 
 function AvailableMeals() {
   const [burgers, setBurgers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchBurgers = async () => {
-      const response = await fetch("https://burgersdata-default-rtdb.europe-west1.firebasedatabase.app/burgers.json");
+      const response = await fetch(
+        "https://burgersdata-default-rtdb.europe-west1.firebasedatabase.app/burgers.json"
+      );
       const responseData = await response.json();
 
       const loadedMeals = [];
 
       for (const key in responseData) {
-        loadedMeals.push(
-          {
-            id: key,
-            name: responseData[key].name,
-            image: responseData[key].image,
-            description: responseData[key].description,
-            price: responseData[key].price
-          }
-        )
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          image: responseData[key].image,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
       }
       setBurgers(loadedMeals);
-    }
+      setLoading(false);
+    };
 
     fetchBurgers();
-  }, [])
+  }, []);
+
+  if (loading) {
+    return (
+      <section className={classes.mealsLoading}>
+        <p>Loading...</p>
+      </section>
+    )
+  }
+
   return (
     <section className={classes.meals}>
       <Card>
         <ul>
-          
           {burgers.map((burger) => {
             return (
-              <MealItem key={burger.id}
+              <MealItem
+                key={burger.id}
                 id={burger.id}
                 name={burger.name}
                 image={burger.image}
