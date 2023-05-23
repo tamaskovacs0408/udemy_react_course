d0c14929380f235d1cdcbcf56afb383b1c9c6b53
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-05-23 16:31:21 +0200

    Add validation to form

--- section_11/src/components/Cart/Checkout.jsx
+++ section_11/src/components/Cart/Checkout.jsx
@@ -1,11 +1,21 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./Checkout.module.css";
 
+const isEmpty = (value) => value.trim() === "";
+const isFiveChars = (value) => value.trim().length === 5;
+
 const Checkout = ({ closeCart }) => {
-    const nameInputRef = useRef();
-    const streetInputRef = useRef();
-    const postalInputRef = useRef();
-    const cityInputRef = useRef();
+    const [formInputsValidity, setFormInputsValidity] = useState({
+        name: true,
+        street: true,
+        city: true,
+        postalCode: true
+    });
+
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalInputRef = useRef();
+  const cityInputRef = useRef();
 
   const handleConfirm = (event) => {
     event.preventDefault();
@@ -14,25 +24,47 @@ const Checkout = ({ closeCart }) => {
     const enteredStreet = streetInputRef.current.value;
     const enteredPostal = postalInputRef.current.value;
     const enteredCity = cityInputRef.current.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredStreetIsValid = !isEmpty(enteredStreet);
+    const enteredCityIsValid = !isEmpty(enteredCity);
+    const enteredPostalIsValid = isFiveChars(enteredPostal);
+
+    setFormInputsValidity({
+        name: enteredNameIsValid,
+        street: enteredStreetIsValid,
+        city: enteredCityIsValid,
+        postalCode: enteredPostalIsValid
+    })
+
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredStreetIsValid &&
+      enteredCityIsValid &&
+      enteredPostalIsValid;
+
+    if (!formIsValid) {
+        return;
+    }
   };
 
   return (
     <form onSubmit={handleConfirm}>
       <div className={classes.control}>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" ref={nameInputRef}/>
+        <input type="text" id="name" ref={nameInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="street">Street</label>
-        <input type="text" id="street" ref={streetInputRef}/>
+        <input type="text" id="street" ref={streetInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal" ref={postalInputRef}/>
+        <input type="text" id="postal" ref={postalInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="city">City</label>
-        <input type="text" id="city" ref={cityInputRef}/>
+        <input type="text" id="city" ref={cityInputRef} />
       </div>
       <button type="button" onClick={closeCart}>
         Cancel
