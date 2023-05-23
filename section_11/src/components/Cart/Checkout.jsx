dfd21774fa21d130b7cc3856b497f2f61bdf2046
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-05-23 15:05:43 +0200

    Add references to validate form

--- section_11/src/components/Cart/Checkout.jsx
+++ section_11/src/components/Cart/Checkout.jsx
@@ -1,27 +1,38 @@
+import { useRef } from "react";
 import classes from "./Checkout.module.css";
 
 const Checkout = ({ closeCart }) => {
+    const nameInputRef = useRef();
+    const streetInputRef = useRef();
+    const postalInputRef = useRef();
+    const cityInputRef = useRef();
+
   const handleConfirm = (event) => {
     event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostal = postalInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
   };
 
   return (
     <form onSubmit={handleConfirm}>
       <div className={classes.control}>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={nameInputRef}/>
       </div>
       <div className={classes.control}>
         <label htmlFor="street">Street</label>
-        <input type="text" id="street" />
+        <input type="text" id="street" ref={streetInputRef}/>
       </div>
       <div className={classes.control}>
         <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal" />
+        <input type="text" id="postal" ref={postalInputRef}/>
       </div>
       <div className={classes.control}>
         <label htmlFor="city">City</label>
-        <input type="text" id="city" />
+        <input type="text" id="city" ref={cityInputRef}/>
       </div>
       <button type="button" onClick={closeCart}>
         Cancel
