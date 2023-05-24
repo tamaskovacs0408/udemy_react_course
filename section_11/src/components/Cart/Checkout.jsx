e226905141101a57e75c1f9dfff60ba0ba42fc64
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-05-24 13:52:20 +0200

    Update form validation

--- section_11/src/components/Cart/Checkout.jsx
+++ section_11/src/components/Cart/Checkout.jsx
@@ -5,12 +5,12 @@ const isEmpty = (value) => value.trim() === "";
 const isFiveChars = (value) => value.trim().length === 5;
 
 const Checkout = ({ closeCart }) => {
-    const [formInputsValidity, setFormInputsValidity] = useState({
-        name: true,
-        street: true,
-        city: true,
-        postalCode: true
-    });
+  const [formInputsValidity, setFormInputsValidity] = useState({
+    name: true,
+    street: true,
+    city: true,
+    postalCode: true,
+  });
 
   const nameInputRef = useRef();
   const streetInputRef = useRef();
@@ -31,11 +31,11 @@ const Checkout = ({ closeCart }) => {
     const enteredPostalIsValid = isFiveChars(enteredPostal);
 
     setFormInputsValidity({
-        name: enteredNameIsValid,
-        street: enteredStreetIsValid,
-        city: enteredCityIsValid,
-        postalCode: enteredPostalIsValid
-    })
+      name: enteredNameIsValid,
+      street: enteredStreetIsValid,
+      city: enteredCityIsValid,
+      postalCode: enteredPostalIsValid,
+    });
 
     const formIsValid =
       enteredNameIsValid &&
@@ -44,27 +44,49 @@ const Checkout = ({ closeCart }) => {
       enteredPostalIsValid;
 
     if (!formIsValid) {
-        return;
+      return;
     }
   };
 
   return (
     <form onSubmit={handleConfirm}>
-      <div className={classes.control}>
+      <div
+        className={`${classes.control} ${
+          formInputsValidity.name ? "" : classes.invalid
+        }`}
+      >
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" ref={nameInputRef} />
+        {!formInputsValidity.name && <p>Please enter a valid name!</p>}
       </div>
-      <div className={classes.control}>
+      <div
+        className={`${classes.control} ${
+          formInputsValidity.street ? "" : classes.invalid
+        }`}
+      >
         <label htmlFor="street">Street</label>
         <input type="text" id="street" ref={streetInputRef} />
+        {!formInputsValidity.street && <p>Please enter a valid street!</p>}
       </div>
-      <div className={classes.control}>
+      <div
+        className={`${classes.control} ${
+          formInputsValidity.postalCode ? "" : classes.invalid
+        }`}
+      >
         <label htmlFor="postal">Postal Code</label>
         <input type="text" id="postal" ref={postalInputRef} />
+        {!formInputsValidity.postalCode && (
+          <p>Please enter a valid postal code! (5 characters long)</p>
+        )}
       </div>
-      <div className={classes.control}>
+      <div
+        className={`${classes.control} ${
+          formInputsValidity.city ? "" : classes.invalid
+        }`}
+      >
         <label htmlFor="city">City</label>
         <input type="text" id="city" ref={cityInputRef} />
+        {!formInputsValidity.city && <p>Please enter a valid city!</p>}
       </div>
       <button type="button" onClick={closeCart}>
         Cancel
