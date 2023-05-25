fe18ce3a95c863d472350b2fe6ca41fc7ce1d8b7
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-05-25 09:25:54 +0200

    Update data fetching url

--- section_14_next_steps/src/App.js
+++ section_14_next_steps/src/App.js
@@ -13,7 +13,7 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://react-http-f2e3a-default-rtdb.europe-west1.firebasedatabase.app/movies.json');
+      const response = await fetch(`${process.env.REACT_APP_FIREBASE_URL}/movies.json`);
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
