3c717b700f0aa8bbd96d90d7926d1a1c1dbe4016
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-05-26 13:53:02 +0200

    Add error page

---
+++ section_20/src/pages/Error.js
@@ -0,0 +1,15 @@
+import MainNav from "../components/MainNav"
+
+const Error = () => {
+  return (
+    <>
+    <MainNav />
+    <main>
+        <h1>An error occurred!</h1>
+        <p>This page couldn't be found.</p>
+    </main>
+    </>
+  )
+}
+
+export default Error
