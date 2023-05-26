aee932144c59767cbcf6304aeeb0dde4bd0c3548
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-05-26 13:53:08 +0200

    Update

--- section_20/src/pages/Layout.js
+++ section_20/src/pages/Layout.js
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import MainNav from "../components/MainNav";
 
-import classes from "./Layout.module.scss";
-
 const Layout = () => {
   return (
     <>
       <MainNav />
-      <main className={classes.content}>
+      <main>
         <Outlet />
       </main>
     </>
