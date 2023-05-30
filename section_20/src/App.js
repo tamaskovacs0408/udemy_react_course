15c3ef20130b260d559b83b327083b93767a53ef
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-05-30 10:42:45 +0200

    Set up error page route

--- section_20/src/App.js
+++ section_20/src/App.js
@@ -2,11 +2,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import Layout from "./pages/Layout";
+import Error from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <Error />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
