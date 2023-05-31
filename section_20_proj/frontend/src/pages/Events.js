6e87b64eae874f1416cda11c4c1437ce0b5cd199
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-05-31 15:56:38 +0200

    Render events

--- section_20_proj/frontend/src/pages/Events.js
+++ section_20_proj/frontend/src/pages/Events.js
@@ -1,7 +1,37 @@
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "Event One",
+  },
+  {
+    id: "e2",
+    title: "Event Two",
+  },
+  {
+    id: "e3",
+    title: "Event Three",
+  },
+  {
+    id: "e4",
+    title: "Event Four",
+  },
+];
+
 const EventsPage = () => {
   return (
-    <div>EventsPage</div>
-  )
-}
+    <>
+      <h1>Events Page</h1>
+      <ul>
+        {DUMMY_EVENTS.map((event) => (
+          <li key={event.id}>
+            <Link to={`/events/${event.id}`}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
-export default EventsPage
+export default EventsPage;
