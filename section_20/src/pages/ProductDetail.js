fddda6ac4e9605250c153280fe651c94684389d2
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-05-31 10:54:53 +0200

    create product detail page

---
+++ section_20/src/pages/ProductDetail.js
@@ -0,0 +1,9 @@
+import { useParams } from "react-router-dom";
+
+const ProductDetail = () => {
+    const params = useParams();
+  
+    return <h1>Product Details</h1>;
+};
+
+export default ProductDetail;
