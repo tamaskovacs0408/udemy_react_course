199f7110b7be0e5df3ebdbbb37f4781c9694cb88
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-05-31 16:01:44 +0200

    Add params to events

--- section_20_proj/frontend/src/pages/EventDetail.js
+++ section_20_proj/frontend/src/pages/EventDetail.js
@@ -1,7 +1,13 @@
-const EventDetailPage = () => {
-  return (
-    <div>EventDetailPage</div>
-  )
-}
+import { useParams } from "react-router-dom";
 
-export default EventDetailPage
+const EventDetailPage = () => {
+  const params = useParams();
+
+  return <>
+  
+    <h1>Event Details</h1>
+    <p>Event id: {params.eventId}</p>
+  </>
+};
+
+export default EventDetailPage;
