c293f4fbc5950530a466adb5aab9ba25ece3a8ab
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-05-31 11:58:29 +0200

    Update products rendering

--- section_20/src/pages/Products.js
+++ section_20/src/pages/Products.js
@@ -1,16 +1,24 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+
+const PRODUCTS = [
+  {id: "p1", title: "Product 1"},
+  {id: "p2", title: "Product 2"},
+  {id: "p3", title: "Product 3"}
+]
 
 const ProductsPage = () => {
   return (
     <>
-        <h1>Products</h1>
-        <ul>
-          <li>Product 1</li>
-          <li>Product 2</li>
-          <li>Product 3</li>
-        </ul>
+      <h1>Products</h1>
+      <ul>
+        {PRODUCTS.map(product => (
+          <li key={product.id}>
+            <Link to={`/products/${product.id}`}>{product.title}</Link>
+          </li>
+        ))}
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default ProductsPage
+export default ProductsPage;
