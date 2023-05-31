520d69a59799f33559e2b4eedbdefd44332ddeca
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-05-31 15:20:25 +0200

    Create routes

--- section_20_proj/frontend/src/App.js
+++ section_20_proj/frontend/src/App.js
@@ -19,6 +19,20 @@
 //    Every list item should include a link to the respective EventDetailPage
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/EditEvent";
+import EventsPage from "./pages/Events";
+import EventDetailPage from "./pages/EventDetail";
+import NewEventPage from "./pages/NewEvent";
+import EditEventPage from "./pages/EditEvent";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/events", element: <EventsPage /> },
+  { path: "/events/:eventId", element: <EventDetailPage /> },
+  { path: "/events/new", element: <NewEventPage /> },
+  { path: "/events/:eventId/edit", element: <EditEventPage /> },
+]);
 
 function App() {
   return <div></div>;
