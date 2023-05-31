b2c4acfa14741afdb6c47949ffb5e2f5bba43aab
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-05-31 10:30:36 +0200

    Add useNavigation hook example

--- section_20/src/pages/Home.js
+++ section_20/src/pages/Home.js
@@ -1,10 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate("/products");
+  }
+
   return (
     <>
       <h1>Homepage</h1>
       <Link to="/products">Go to products</Link>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
+      </p>
     </>
   );
 };
