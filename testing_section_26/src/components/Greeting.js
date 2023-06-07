7e60c845ba2c2c9fa1fdca141957859d79fe509b
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-06-07 15:43:29 +0200

    Import component

--- testing_section_26/src/components/Greeting.js
+++ testing_section_26/src/components/Greeting.js
@@ -1,10 +1,22 @@
+import { useState } from "react";
+import Output from "./Output";
+
 const Greeting = () => {
+  const [changedText, setChangedText] = useState(false);
+
+  const changedTextHandler = () => {
+    setChangedText(true);
+  }
+
   return (
     <div>
-        <h1>Hello World!</h1>
-        <p>It's good to see you!</p>
+      <h1>Hello World!</h1>
+      {!changedText && <Output>It's good to see you!</Output>}
+      {changedText && <Output>Changed!</Output>}
+      <button onClick={changedTextHandler}>Change text</button>
     </div>
-  )
-}
+  );
+};
 
-export default Greeting
+
+export default Greeting;
