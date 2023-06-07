7c1f8d05a559906b817fd0872931ca0aea6dbdba
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-06-07 15:43:11 +0200

    Add more tests

--- testing_section_26/src/components/Greeting.test.js
+++ testing_section_26/src/components/Greeting.test.js
@@ -1,8 +1,25 @@
-import { render, screen} from '@testing-library/react';
-import Greeting from "./Greeting"
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Greeting from "./Greeting";
 
-test('Rendering Hello Word', () => {
-     render(<Greeting />);
-     
-     expect(screen.getByText(/Hello World/i)).toBeInTheDocument();
-})
+describe("Testing Greeting component", () => {
+  test("rendering Hello Word", () => {
+    render(<Greeting />);
+
+    expect(screen.getByText(/Hello World/i)).toBeInTheDocument();
+  });
+
+  test('rendering "good to see you" when button NOT clicked', () => {
+    render(<Greeting />);
+
+    expect(screen.getByText(/good to see you/i)).toBeInTheDocument();
+  });
+
+  test('rendering "Changed!" when button clicked', () => {
+    render(<Greeting />);
+    // Simulating the button clicking with userEvent, but we can use fireEvent too
+    userEvent.click(screen.getByRole("button"));
+
+    expect(screen.getByText("Changed!")).toBeInTheDocument();
+  });
+});
