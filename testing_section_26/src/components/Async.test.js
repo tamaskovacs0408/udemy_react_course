ced4f8a4285a1eecd2b2258e8720092fa3d16b88
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-06-07 16:26:00 +0200

    Create mock function

--- testing_section_26/src/components/Async.test.js
+++ testing_section_26/src/components/Async.test.js
@@ -4,11 +4,20 @@ import Async from "./Async";
 
 describe('Testing Async component', () => {
     test('renders posts if request succeeds', async () => {
-        render(<Async />);
         /* 
-        We have to use in that case (requesting data from endpoint) the finAllByRole, 
+        ----- USE MOCKS TO TEST THE HTTP REQUEST INSTEAD OF THE REAL ENDPOINTS! ------
+       Override the component's http request (fetch) with a dummy fetch function witch is set to
+       take only an id and a title.
+        */
+       window.fetch = jest.fn();
+       window.fetch.mockResolvedValueOnce({
+        json: async () => [{id: 'p1', title: 'First post'}]
+       });
+        render(<Async />);
+        /*
+         We have to use in that case (requesting data from endpoint) the finAllByRole, 
         because it returns a Promise that we have to handle with async/await. The findAllByRole
-        can have more arguments and the 3rd argument is the timeout which is 1000ms by default
+        can have more arguments and the 3rd argument is the timeout which is 1000ms by default.
         */
         const listElement = await screen.findAllByRole('listitem');   
         expect(listElement).not.toHaveLength(0);
