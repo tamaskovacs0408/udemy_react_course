a613a80a70ffba031199f7f9ad289d42fa769573
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-06-21 10:52:11 +0200

    Change to arrow function

--- section_20_proj/frontend/src/components/PageContent.js
+++ section_20_proj/frontend/src/components/PageContent.js
@@ -1,6 +1,5 @@
 import classes from './PageContent.module.scss';
-
-function PageContent({ title, children }) {
+const PageContent = ({ title, children }) => {
   return (
     <div className={classes.content}>
       <h1>{title}</h1>
