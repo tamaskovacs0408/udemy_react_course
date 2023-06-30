22fa6115b49cbcfed9088a244b631f6d9d1f8c35
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-06-30 22:16:08 +0200

    Add new logic to throw error

--- section_20_proj/frontend/src/pages/Error.js
+++ section_20_proj/frontend/src/pages/Error.js
@@ -1,7 +1,26 @@
-const Error = () => {
+import { useRouteError } from "react-router-dom"
+import PageContent from "../components/PageContent"
+
+const ErrorPage = () => {
+  const error = useRouteError();
+
+  let title = "An error occurred";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    title = "Not found"
+    message = "Couldn't find resource or page.";
+  }
+
   return (
-    <h1>An error ocurred!</h1>
+    <PageContent title={title}>
+      <p>{message}</p>
+    </PageContent>
   )
 }
 
-export default Error
+export default ErrorPage
