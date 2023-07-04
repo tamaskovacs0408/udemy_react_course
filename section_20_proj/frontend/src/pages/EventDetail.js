f0faa2f8a23e4a5f260558f3ee42f1205d510dc5
AzureAD\Tamaskovacs <tamas.kovacs@prefixbox.com>
2023-07-04 14:05:22 +0200

    Create loader fn for fetching event details

--- section_20_proj/frontend/src/pages/EventDetail.js
+++ section_20_proj/frontend/src/pages/EventDetail.js
@@ -1,13 +1,22 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 const EventDetailPage = () => {
-  const params = useParams();
 
-  return <>
-  
-    <h1>Event Details</h1>
-    <p>Event id: {params.eventId}</p>
-  </>
+  const data = useLoaderData();
+
+  return <EventItem event={data.event}/>;
 };
 
 export default EventDetailPage;
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + id);
+  
+  if(!response.ok) {
+    return json({ message: "Couldn't fetch details for this events." }, { status: 500 });
+  } else {
+    return response;
+  }
+}
