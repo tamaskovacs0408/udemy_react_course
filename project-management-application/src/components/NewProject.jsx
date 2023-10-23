cb85737cdda4755feead96879f2251647d37ffab
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-10-23 16:10:33 +0200

    Add tailwind styles

--- project-management-application/src/components/NewProject.jsx
+++ project-management-application/src/components/NewProject.jsx
@@ -2,8 +2,8 @@ import Input from "./Input";
 
 const NewProject = () => {
   return (
-    <div>
-      <menu>
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 my-4">
         <li>
           <button>Cancel</button>
         </li>
