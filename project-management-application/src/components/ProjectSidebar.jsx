c411b8029db21374f610f20660c05342e170a626
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-10-23 13:42:36 +0200

    Create sidebar component

---
+++ project-management-application/src/components/ProjectSidebar.jsx
@@ -0,0 +1,11 @@
+const ProjectSidebar = () => {
+  return (
+    <aside>
+      <h2 className="tetxt-center text-xl font-bold">Your projects</h2>
+      <div><button>+ Add Projects</button></div>
+      <ul></ul>
+    </aside>
+  )
+}
+
+export default ProjectSidebar
