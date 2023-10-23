06b64871ba53d393bd9a9971e36b52c63a03dc68
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-10-23 13:45:05 +0200

    Delete unused code

--- project-management-application/src/App.jsx
+++ project-management-application/src/App.jsx
@@ -3,8 +3,7 @@ import ProjectSidebar from "./components/ProjectSidebar";
 function App() {
   return (
     <>
-    <ProjectSidebar />
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <ProjectSidebar />
     </>
   );
 }
