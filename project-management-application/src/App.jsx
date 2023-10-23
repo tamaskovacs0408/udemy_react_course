136ea752d8990eefb505ec96b11bd10accc9ba4d
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-10-23 13:56:49 +0200

    Add basic styles with tailwind css

--- project-management-application/src/App.jsx
+++ project-management-application/src/App.jsx
@@ -2,9 +2,9 @@ import ProjectSidebar from "./components/ProjectSidebar";
 
 function App() {
   return (
-    <>
+    <main className="h-screen my-8">
       <ProjectSidebar />
-    </>
+    </main>
   );
 }
 
